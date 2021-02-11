45ad4fbbe2750880da1a945ce21e6792122a27a7
Mark Kvetny <m.kvetny@outlook.com>
2021-02-12 00:04:41 +0100

    give it a *desktop* look

--- src/app/seven_guis.cljs
+++ src/app/seven_guis.cljs
@@ -7,27 +7,51 @@
    [app.seven-guis.crud :refer [crud]]
    [app.seven-guis.circle-drawer :refer [circle-drawer]]
    [app.seven-guis.cells :refer [cells letters rows]]
-   [app.window :refer [win-98-window]]))
+   [app.window :refer [win-98-window]]
+   [reagent.core :as r]))
+
+(declare components)
+
+(def open (r/atom nil))
+(defn on-close [] (reset! open nil))
+
+(def components {:counter [win-98-window {:title "counter"
+                                          :link "https://github.com/biowaffeln/seven-guis-reagent/blob/master/src/app/seven_guis/counter.cljs"
+                                          :on-close on-close}
+                           [counter]]
+                 :temp    [win-98-window {:title "temperature converter"
+                                          :on-close on-close}
+                           [temperature-converter]]
+                 :flight  [win-98-window {:title "flight booker"
+                                          :on-close on-close}
+                           [flight-booker]]
+                 :timer   [win-98-window {:title "timer"
+                                          :on-close on-close}
+                           [timer]]
+                 :crud    [win-98-window {:title "crud"
+                                          :on-close on-close}
+                           [crud]]
+                 :draw  [win-98-window {:title "circle-drawer"
+                                        :on-close on-close}
+                         [circle-drawer]]
+                 :spread  [:div
+                           [:p.description "Try some of these formulas: `=2*5+5`, `=A5/10`,
+                            `=SUM(A1:B5)`, `=5*AVG(A1:A5)`"]
+                           [win-98-window {:title "cells" :class "cells"
+                                           :on-close on-close}
+                            [cells letters rows]]]})
+
 
 (defn app []
   [:<>
    [:h1 "7GUIs in ClojureScript/Reagent"]
+   [:a.about {:href "https://eugenkiss.github.io/7guis/"}
+    "about the challenge"]
+   [:div.icons
+    (for [[name component] components]
+      ^{:key name} [:div.icon
+                    {:on-click #(reset! open component)}
+                    [:img {:src "/icon.png"}]
+                    [:p name]])]
    [:div.container
-
-    [win-98-window {:title "counter"}
-     [counter]]
-    [win-98-window {:title "temperature converter"}
-     [temperature-converter]]
-    [win-98-window {:title "flight booker"}
-     [flight-booker]]
-    [win-98-window {:title "timer"}
-     [timer]]
-    [win-98-window {:title "crud"}
-     [crud]]
-    [win-98-window {:title "circle-drawer"}
-     [circle-drawer]]
-    [:div
-     [:p.description "Try some of these formulas: `=2*5+5`, `=A5/10`,
-                      `=SUM(A1:B5)`, `=5*AVG(A1:A5)`"]
-     [win-98-window {:title "cells" :class "cells"}
-      [cells letters rows]]]]])
+    @open]])
--- src/app/window.cljs
+++ src/app/window.cljs
@@ -7,9 +7,9 @@
    [:div.title-bar
     [:div.title-bar-text (:title props)]
     [:div.title-bar-controls
-     [:button {:aria-label "Minimize"}]
-     [:button {:aria-label "Maximize"}]
-     [:button {:aria-label "Close"}]]]
+     [:a {:href (:link props) :target "_blank"}
+      [:button {:aria-label "Help"}]]
+     [:button {:aria-label "Close" :on-click (:on-close props)}]]]
    [:div.window-body
     (into [:<>] (r/children (r/current-component)))]])
 
